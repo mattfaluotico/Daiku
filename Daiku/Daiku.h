f74caf8919bcfbfd60149971745aee361a65ce60
mattfxyz <matt.faluotico@gmail.com>
2017-03-18 10:24:44 -0700

    xcode project

---
+++ Daiku/Daiku.h
@@ -0,0 +1,19 @@
+//
+//  Daiku.h
+//  Daiku
+//
+//  Created by Matthew Faluotico on 3/18/17.
+//  Copyright © 2017 mattfaluotico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Daiku.
+FOUNDATION_EXPORT double DaikuVersionNumber;
+
+//! Project version string for Daiku.
+FOUNDATION_EXPORT const unsigned char DaikuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Daiku/PublicHeader.h>
+
+
